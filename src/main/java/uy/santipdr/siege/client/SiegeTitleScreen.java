d8ac3dfb600ff530af959ef8646701b63a884c35
Santi.pdr <santiagopereiraxd029@gmail.com>
2026-09-08 01:04:17 -0300

    fix: use Forge 1.20.1 OptionsScreen package

--- src/main/java/uy/santipdr/siege/client/SiegeTitleScreen.java
+++ src/main/java/uy/santipdr/siege/client/SiegeTitleScreen.java
@@ -5,7 +5,7 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen;
-import net.minecraft.client.gui.screens.options.OptionsScreen;
+import net.minecraft.client.gui.screens.OptionsScreen;
 import net.minecraft.client.gui.screens.worldselection.SelectWorldScreen;
 import net.minecraft.network.chat.Component;
 import org.lwjgl.glfw.GLFW;
