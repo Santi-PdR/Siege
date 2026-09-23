9661623e718c20462774c3a9123f5d16ec874ba3
Santi.pdr <santiagopereiraxd029@gmail.com>
2026-09-23 16:24:46 -0300

    feat: register Stronghold Black Signal music

--- src/main/java/uy/santipdr/siege/SiegeMod.java
+++ src/main/java/uy/santipdr/siege/SiegeMod.java
@@ -18,6 +18,7 @@ public final class SiegeMod {
     public static final RegistryObject<SoundEvent> KAPTAIN_MUSIC_BOX = sound("music.kaptain_music_box");
     public static final RegistryObject<SoundEvent> HEAVENS_GIFT = sound("music.heavens_hell_sent_gift");
     public static final RegistryObject<SoundEvent> ARC_ENEMY = sound("music.arc_enemy");
+    public static final RegistryObject<SoundEvent> STRONGHOLD_BLACK_SIGNAL = sound("music.stronghold_black_signal");
     public static final RegistryObject<SoundEvent> UI_HOVER = sound("ui.hover");
     public static final RegistryObject<SoundEvent> UI_CLICK = sound("ui.click");
     public static final RegistryObject<SoundEvent> UI_BACK = sound("ui.back");
